55c69b8a8adc3b89d110ba7ce90478867b9899ee
WiltencirDG <wiltencir@gmail.com>
2019-05-19 15:39:59 -0300

    Starting alocation

--- app/src/main/java/com/schelas/schelasvans/controller/Alocar.java
+++ app/src/main/java/com/schelas/schelasvans/controller/Alocar.java
@@ -3,8 +3,10 @@ package com.schelas.schelasvans.controller;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.schelas.schelasvans.R;
 
@@ -13,6 +15,9 @@ public class Alocar extends AppCompatActivity {
 
     private Button btnAlocar;
     private ImageView ivToolbar;
+    private Spinner spVeic;
+    private Spinner spPass;
+    private Spinner spDest;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -24,7 +29,9 @@ public class Alocar extends AppCompatActivity {
     }
 
     private void setUI(){
-
+        spVeic = findViewById(R.id.spinner1);
+        spPass = findViewById(R.id.spinner2);
+        spDest = findViewById(R.id.spinner3);
     }
 
     private void setToolbar(){
